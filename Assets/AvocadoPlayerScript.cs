954e83c47d776f54d2295322fde8aba708da4dd0
Tibau vdb <tibau.van.den.broeck@student.howest.be>
2019-04-23 17:34:42 +0200

    Added Shader to Special Attack Avocado
    Also Extracted Avocado Clips from Prefab

---
+++ Assets/AvocadoPlayerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvocadoPlayerScript : MonoBehaviour
+{
+    public GameObject FireGameobject;
+    [SerializeField] private float _lerpSpeed;
+    private Material _fireMaterial;
+
+    private bool _startFireShader = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _fireMaterial = FireGameobject.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_startFireShader)
+            LerpFireShader();
+        else
+            ResetFireShader();
+
+    }
+
+    public void setBool()
+    {
+        _startFireShader = !_startFireShader;
+    }
+
+    public void LerpFireShader()
+    {
+        _fireMaterial.SetFloat("Vector1_E1258AB5", lerpValue(_fireMaterial.GetFloat("Vector1_E1258AB5"), 0));
+    }
+
+    public void ResetFireShader()
+    {
+        _fireMaterial.SetFloat("Vector1_E1258AB5", lerpValue(_fireMaterial.GetFloat("Vector1_E1258AB5"), .5f));
+    }
+
+    private float lerpValue(float lerpThis, float endValue)
+    {
+        lerpThis = Mathf.Lerp(lerpThis, endValue, Time.deltaTime * _lerpSpeed);
+        return lerpThis;
+    }
+}
