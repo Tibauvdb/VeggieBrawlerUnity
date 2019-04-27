26a99f0adaac71aea214a4097c6dfa280477cfa3
OscarTulkens <43540793+OscarTulkens@users.noreply.github.com>
2019-04-27 23:52:45 +0200

    Revert "Merge branch 'master' of https://github.com/Tibauvdb/VeggieBrawlerUnity"
    This reverts commit 19c88939f1be448430f37c8a09ee967f11a2f69f, reversing
    changes made to 269107f3055cc28886312e68ace3f768058ed916.

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -10,6 +10,9 @@ public class MusicManager : MonoBehaviour
     [SerializeField] private AudioSource _CharSel;
     [SerializeField] private AudioSource _Fight;
 
+    [SerializeField] private float _soundChangeSpeedValue;
+
+    private bool _gameDone;
 
 
     // Start is called before the first frame update
@@ -24,32 +27,54 @@ public class MusicManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "MainMenu" && _Menu.volume != 1)
+        
+        if (SceneManager.GetActiveScene().name == "MainMenu")
         {
-            _Menu.volume = Mathf.MoveTowards(_Menu.volume, 1, 0.15f*Time.deltaTime);
-            _CharSel.volume = Mathf.MoveTowards(_CharSel.volume, 0, 0.3f*Time.deltaTime);
-            _Fight.volume = Mathf.MoveTowards(_Fight.volume, 0, 0.3f*Time.deltaTime);
+            if (_gameDone == true)
+            {
+                if (_Menu.volume>=0.2f)
+                {
+                    _Menu.volume = Mathf.MoveTowards(_Menu.volume, 0, _soundChangeSpeedValue*2 * Time.deltaTime);
+                }
+                else
+                {
+                    Destroy(this.gameObject);
+                }
+
+            }
+
+            else if (_Menu.volume != 1)
+            {
+                _Menu.volume = Mathf.MoveTowards(_Menu.volume, 1, _soundChangeSpeedValue / 2 * Time.deltaTime);
+                _CharSel.volume = Mathf.MoveTowards(_CharSel.volume, 0, _soundChangeSpeedValue * Time.deltaTime);
+                _Fight.volume = Mathf.MoveTowards(_Fight.volume, 0, _soundChangeSpeedValue * Time.deltaTime);
+            }
+
         }
 
         else if (SceneManager.GetActiveScene().name == "CharacterSelect" && _CharSel.volume !=1)
         {
-            _Menu.volume = Mathf.MoveTowards(_Menu.volume, 0, 0.3f * Time.deltaTime);
-            _CharSel.volume = Mathf.MoveTowards(_CharSel.volume, 1, 0.15f * Time.deltaTime);
-            _Fight.volume = Mathf.MoveTowards(_Fight.volume, 0, 0.3f * Time.deltaTime);
+            _Menu.volume = Mathf.MoveTowards(_Menu.volume, 0, _soundChangeSpeedValue * Time.deltaTime);
+            _CharSel.volume = Mathf.MoveTowards(_CharSel.volume, 1, _soundChangeSpeedValue/2 * Time.deltaTime);
+            _Fight.volume = Mathf.MoveTowards(_Fight.volume, 0, _soundChangeSpeedValue * Time.deltaTime);
         }
 
         else if (SceneManager.GetActiveScene().name == "GameScene" && _Fight.volume != 1)
         {
-            _Menu.volume = Mathf.MoveTowards(_Menu.volume, 0, 0.3f *Time.deltaTime);
-            _CharSel.volume = Mathf.MoveTowards(_CharSel.volume, 0, 0.3f * Time.deltaTime);
-            _Fight.volume = Mathf.MoveTowards(_Fight.volume, 1, 0.15f * Time.deltaTime);
+            _Menu.volume = Mathf.MoveTowards(_Menu.volume, 0, _soundChangeSpeedValue *Time.deltaTime);
+            _CharSel.volume = Mathf.MoveTowards(_CharSel.volume, 0, _soundChangeSpeedValue * Time.deltaTime);
+            _Fight.volume = Mathf.MoveTowards(_Fight.volume, 1, _soundChangeSpeedValue/2 * Time.deltaTime);
         }
 
         else if (SceneManager.GetActiveScene().name == "WinScreen" && _Menu.volume != 1)
         {
-            _Menu.volume = Mathf.MoveTowards(_Menu.volume, 1, 0.15f * Time.deltaTime);
-            _CharSel.volume = Mathf.MoveTowards(_CharSel.volume, 0, 0.3f * Time.deltaTime);
-            _Fight.volume = Mathf.MoveTowards(_Fight.volume, 0, 0.3f * Time.deltaTime);
+            if (_gameDone == false)
+            {
+                _gameDone = true;
+            }
+            _Menu.volume = Mathf.MoveTowards(_Menu.volume, 1, _soundChangeSpeedValue/2 * Time.deltaTime);
+            _CharSel.volume = Mathf.MoveTowards(_CharSel.volume, 0, _soundChangeSpeedValue * Time.deltaTime);
+            _Fight.volume = Mathf.MoveTowards(_Fight.volume, 0, _soundChangeSpeedValue * Time.deltaTime);
         }
     }
 }
